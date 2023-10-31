5f3f7f69a00a8ade69c0e83477d9eed97a430840
kevinnamkung <71405245+kevinnamkung@users.noreply.github.com>
2023-10-31 19:14:38 -0400

    template for teamdb code. userdb code used for now

---
+++ teamdb.php
@@ -0,0 +1,55 @@
+<?php
+function addTeam($firstname, $lastname, $email) 
+{
+//   global $db; 
+// //   $query = "insert into friends values ('" . $friendname . "', '" . $major . "'," . $year .") ";
+//   //=$db->query($query);  // compile + exe
+
+//   $query = "INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)";
+//   // prepare: 
+//   // 1. prepare (compile) 
+//   // 2. bindValue + exe
+
+//   $statement = $db->prepare($query); 
+//   $statement->bindValue(':firstname', $firstname);
+//   $statement->bindValue(':lastname', $lastname);
+//   $statement->bindValue(':email', $email);
+//   $statement->execute();
+//   $statement->closeCursor();
+}
+
+// function getAllTeams()
+// {
+//   global $db;
+//   $query = "select * from users";
+//   $statement = $db->prepare($query); 
+//   $statement->execute();
+//   $results = $statement->fetchAll();   // fetch()
+//   $statement->closeCursor();
+//   return $results;
+// }
+
+// function updateTeam($firstname, $lastname , $email)
+// {
+//     global $db; 
+//     $query = "update users set firstname=:firstname, lastname=:lastname where email=:email";
+
+//     $statement = $db->prepare($query); 
+//     $statement->bindValue(':firstname', $firstname);
+//     $statement->bindValue(':lastname', $lastname);
+//     $statement->bindValue(':email', $email);
+//     $statement->execute();
+//     $statement->closeCursor();
+// }
+
+// function deleteTeam($email)
+// {
+//     global $db; 
+//     $query = "delete from users where email=:email";
+
+//     $statement = $db->prepare($query); 
+//     $statement->bindValue(':email', $email);
+//     $statement->execute();
+//     $statement->closeCursor();
+// }
+// ?>
